7261dce39a860f4aba7d8ec3f27bc751a82594c1
ddavis60 <ddavis60@ggc.edu>
2020-09-30 12:53:42 -0400

    Still fixing nav

--- App.js
+++ App.js
@@ -11,16 +11,19 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import LoginScreen from './Screens/LoginScreen';
+import {View} from 'react-native-web';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="Log In" component={LoginScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <View>
+        <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Log In" component={LoginScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
   );
 }
 
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -2,46 +2,42 @@ import React from 'react';
 import {StyleSheet, TextInput, View, Button} from 'react-native';
 import {Card} from 'react-native-elements';
 
-class LoginScreen extends React.Component{
-    signUp: Button;
-    signIn: Button;
-    render() {
-        return(
-            <View style={styles.login}>
-                <Card style = {styles.loginCard}>
-                    <Card.Title>Let's get these gains!</Card.Title>
-                    <TextInput>Username</TextInput>
-                    <TextInput>Password</TextInput>
-                    <View style = {styles.buttonView}>
-                        <Button style = {styles.signUpButton} title={this.signUp}>Sign Up</Button>
-                        <Button style = {styles.signUpButton} title={this.signIn}>Sign In</Button>
-                    </View>
-                </Card>
-            </View>
-        )
-    }
+function LoginScreen() {
+    return(
+        <View style={styles.login}>
+            <Card style = {styles.loginCard}>
+                <Card.Title>Let's get these gains!</Card.Title>
+                <TextInput>Username</TextInput>
+                <TextInput>Password</TextInput>
+                <View style = {styles.buttonView}>
+                    <Button style = {styles.signUpButton} title={this.signUp}>Sign Up</Button>
+                    <Button style = {styles.signUpButton} title={this.signIn}>Sign In</Button>
+                </View>
+            </Card>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     login: {
-        background: '#1C2224',
+        backgroundColor: '#1C2224',
         alignContent: 'center',
     },
     loginCard: {
         position: 'absolute',
         width: 366,
         height: 30,
-        background: '#404A4F',
+        backgroundColor: '#404A4F',
         alignContent: 'center',
     },
     signUpButton: {
-        background: '066da1',
+        backgroundColor: '066da1',
         color: 'white',
         width: 148,
         height: 37.16,
     },
     signInButton: {
-        background: '5189a5',
+        backgroundColor: '5189a5',
         color: 'white',
         width: 148,
         height: 37.16,
