9205d8f4a8d30a53c3e480337ce501d3ec14e5e6
ddavis60 <ddavis60@ggc.edu>
2020-10-01 19:44:28 -0400

    created empty screens

---
+++ Screens/CreateRoutineScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
+import {Card} from 'react-native-elements';
+
+class CreateRoutineScreen extends Component {
+    render() {
+    }
+}
+
+const styles = StyleSheet.create({
+});
+
+module.exports = CreateRoutineScreen;
---
+++ Screens/FitnessAnalyticsScreen.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
+import {Card} from 'react-native-elements';
+
+class FitnessAnalyticsScreen extends Component {
+    render() {
+    }
+}
+
+const styles = StyleSheet.create({
+});
+
+module.exports = FitnessAnalyticsScreen;
