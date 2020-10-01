f30b043d8c02622bb4f069bf64694f967704a6ff
ddavis60 <ddavis60@ggc.edu>
2020-10-01 18:05:28 -0400

    added package for graphs and started home page

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import LoginScreen from './Screens/LoginScreen';
+import HomeScreen from './Screens/HomeScreen';
 import {enableScreens} from 'react-native-screens';
 
 // run this before any screen render(usually in App.js)
+// <Stack.Screen name="Log In" component={LoginScreen}/> add this shit back later...
 enableScreens();
 
 const Stack = createNativeStackNavigator();
@@ -14,7 +16,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false }}>
-        <Stack.Screen name="Log In" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
