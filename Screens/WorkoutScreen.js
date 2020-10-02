4fcfb420d515a585670ec77e05521d18eddd3a51
Anne <ajoseph13@ggc.edu>
2020-10-02 17:17:23 -0400

    header, title, tabs

--- Screens/WorkoutScreen.js
+++ Screens/WorkoutScreen.js
@@ -1,13 +1,80 @@
 import React, {Component} from 'react';
-import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
-import {Card} from 'react-native-elements';
+import {StyleSheet, Text, View} from 'react-native';
+import {Header} from 'react-native-elements';
+import SegmentedControlTab from 'react-native-segmented-control-tab';
 
 class WorkoutScreen extends Component {
+    constructor(){
+        super();
+        this.state = {
+            selectedIndex: 0
+        };
+    }
+
+    handleIndexChange = index => {
+        this.setState({
+            ...this.state,
+            selectedIndex: index
+        });
+    };
+
     render() {
+        return (
+            <View style={styles.workout}>
+                <Header>
+
+                </Header>
+                <View style={styles.container}>
+                    <Text style={styles.titleText}>Choose a Workout</Text>
+                    <SegmentedControlTab
+                        values={['Area of Focus','Activity','Intensity']}
+                        selectedIndex={this.state.selectedIndex}
+                        onTabPress={this.handleIndexChange}
+                        tabsContainerStyle={styles.tabContainer}
+                        tabStyle={styles.tab}
+                        activeTabStyle={styles.activeTab}
+                        tabTextStyle={styles.tabText }
+                        activeTabTextStyle={{ color: '#888888' }}
+                    />
+                </View>
+            </View>
+        );
     }
 }
 
 const styles = StyleSheet.create({
+    workout: {
+        backgroundColor: '#1C2224',
+        flex: 1,
+    },
+    header: {
+
+    },
+    container: {
+        marginVertical: 30,
+        marginHorizontal: 30,
+    },
+    titleText: {
+        color: 'white',
+        fontSize: 27,
+    },
+    tabContainer: {
+        height: 33,
+        backgroundColor: '#F2F2F2'
+    },
+    tab: {
+        backgroundColor: '#F2F2F2',
+        borderWidth: 0,
+        borderColor: 'transparent',
+    },
+    activeTab: {
+        backgroundColor: 'white',
+        marginTop: 2,
+    },
+    tabText: {
+        color: '#444444',
+        fontWeight: 'bold'
+    }
 });
 
-module.exports = WorkoutScreen;
+module .exports = WorkoutScreen;
