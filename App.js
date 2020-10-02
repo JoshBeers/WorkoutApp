6f1e17de2c36f04dd899aacc933f5e212699fe18
Christipher James Arackal <ChristopherJamesArackal@ChristiphersMBP.attlocal.net>
2020-10-02 19:38:26 -0400

    working on firebase for login page

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from 'react-native-screens/native-stack';
 import LoginScreen from './Screens/LoginScreen';
 import HomeScreen from './Screens/HomeScreen';
-import SignUpScreen from "./Screens/SignUpScreen";
-import WorkoutScreen from "./Screens/WorkoutScreen";
+import SignUpScreen from './Screens/SignUpScreen';
+import WorkoutScreen from './Screens/WorkoutScreen';
 import {enableScreens} from 'react-native-screens';
-
 
 // run this before any screen render(usually in App.js)
 enableScreens();
@@ -17,9 +16,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen}/>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Signup" component={SignUpScreen} />
         <Stack.Screen name="Workout" component={WorkoutScreen} />
       </Stack.Navigator>
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,16 +1,25 @@
 import React, {Component} from 'react';
 import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
 import {Card} from 'react-native-elements';
+import * as firebase from 'firebase';
 
 class LoginScreen extends Component {
   signUp: Button;
   signIn: Button;
-  state = {email: '', password: '' , errorMessage: null}
+  state = {
+    email: '',
+    password: '',
+    errorMessage: null,
+  };
 
   handleLogin = () => {
-    console.log('email = ',this.state.email)
+    const {email, password} = this.state;
 
-  }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => this.setState({errorMessage: error.message}));
+  };
 
   render() {
     return (
@@ -28,25 +37,25 @@ class LoginScreen extends Component {
           </View>
           <View style={styles.textField}>
             <TextInput
-                secureTextEntry
-                placeholder="PASSWORD"
-                style={{marginLeft: 3, marginRight: 3, marginTop: 2}}
-                onChangeText={(password) => this.setState({password})}
-                value={this.state.password}
+              secureTextEntry
+              placeholder="PASSWORD"
+              style={{marginLeft: 3, marginRight: 3, marginTop: 2}}
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password}
             />
           </View>
           <View style={styles.buttonView}>
-            <View style={styles.signUpButton}>
-              <Button color = "#066da1" title="Sign In"
-                      onPress={this.handleLogin}>
-                <Text style={styles.buttonText}>Sign Up</Text>
-              </Button>
-            </View>
             <View style={styles.signInButton}>
               <Button
-                  color = "#5189a5" title="Sign Up"
-              >
+                color="#066da1"
+                title="Sign In"
+                onPress={this.handleLogin}>
                 <Text style={styles.buttonText}>Sign In</Text>
+              </Button>
+            </View>
+            <View style={styles.signUpButton}>
+              <Button color="#5189a5" title="Sign Up">
+                <Text style={styles.buttonText}>Sign Up</Text>
               </Button>
             </View>
           </View>
@@ -106,7 +115,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     borderWidth: 2,
     borderColor: '#404A4F',
-  }
+  },
 });
 
 module.exports = LoginScreen;
