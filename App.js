585bf602d208a118a8c92d2bee270aae6a7c4953
Josh Beers <joshbeers999@gmail.com>
2020-10-09 19:08:38 -0400

    i think the tables should be created

--- App.js
+++ App.js
@@ -5,6 +5,28 @@ import LoginScreen from './Screens/LoginScreen';
 import SignUpScreen from './Screens/SignUpScreen';
 import HomeScreen from './Screens/HomeScreen';
 import WorkoutScreen from './Screens/WorkoutScreen';
+import * as SQLite from 'expo-sqlite';
+
+
+//Sqlite stuff
+
+const db = SQLite.openDatabase("workoutAppDB.db");
+
+db.transaction(tx => {
+    tx.executeSql(
+        //creates workout table
+        "CREATE table Workouts(ID integer primary key DESC, date date not null);" +
+        //creates exercise table
+        "Create table Exercises(ID integer primary key DESC, name varchar(30) not null, description varchar(120));" +
+        //creates completedExercises table
+        "create table CompletedExercises(ID integer primary key DESC, exerciseId int not null,numberOfReps int not null,numberOfSets int not null, workOutID int not null,FOREIGN KEY(exerciseId) REFERENCES Exercises(ID),FOREIGN KEY(workOutID) REFERENCES Workouts(ID));" +
+        //creates WorkoutRoutines tables
+        "create table WorkoutRoutines(ID integer primary key DESC, name varchar(30) not null);" +
+        //creates ExercisesWithinRoutine tables
+        "create table ExercisesWithinRoutines(exerciseID int not null, routineID int not null, numberOFReps int not null, numberOfSets int not Null, placeInOrder int not null, FOREIGN KEY(exerciseId) REFERENCES Exercises(ID),FOREIGN KEY(routineID) REFERENCES WorkoutRoutines(ID), Primary key(exerciseId,routineID));"
+    );
+});
+
 
 
 import * as firebase from 'firebase';
