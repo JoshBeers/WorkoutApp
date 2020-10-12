0b96d89b88ba9b78f71276dc0317c431f7268695
Anne <ajoseph13@ggc.edu>
2020-10-11 20:04:11 -0400

    added functionality to tabs

--- Screens/WorkoutScreen.js
+++ Screens/WorkoutScreen.js
@@ -2,28 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
 import Colors from '../Themes/Colors';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
-import {Arm, Abs, Rear, WorkoutIcons, Back, Chest} from '../img/WorkoutIcons';
-
-const data = [
-  //cardio, weights
-  {title: 'Arms', icon: <Arm />},
-  {title: 'Abs', icon: <Abs />},
-  {title: 'Rear', icon: <Rear />},
-  {title: 'Legs', icon: <WorkoutIcons />},
-  {title: 'Back', icon: <Back />},
-  {title: 'Chest', icon: <Chest />},
-];
-const numColumns = 2;
-const renderItem = ({item, index}) => {
-  return (
-    <View Key={index} style={styles.itemContainer}>
-      <View style={styles.itemLogo}>{item.icon}</View>
-      <View>
-        <Text style={styles.itemTitle}>{item.title}</Text>
-      </View>
-    </View>
-  );
-};
+import {Cardio, Weights} from './WorkoutTabs';
 
 export default class WorkoutScreen extends Component {
   constructor() {
@@ -55,17 +34,7 @@ export default class WorkoutScreen extends Component {
             tabTextStyle={styles.tabText}
             activeTabTextStyle={{color: Colors.textDark}}
           />
-          <FlatList
-            data={data}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={renderItem}
-            selectedIndex={0}
-            // style={styles.gridView}
-            contentContainerStyle={styles.gridView}
-            columnWrapperStyle={{justifyContent: 'space-between'}}
-            spacing={20}
-            numColumns={numColumns}
-          />
+          {this.state.selectedIndex === 0 ? <Cardio /> : <Weights />}
         </View>
       </View>
     );
@@ -106,24 +75,6 @@ const styles = StyleSheet.create({
   },
   gridView: {
     flexDirection: 'column',
-    alignSelf: 'center',
-  },
-  itemContainer: {
-    backgroundColor: Colors.card,
-    alignItems: 'center',
-    margin: 9,
-    width: '45%',
-    height: 160,
-  },
-  itemTitle: {
-    fontWeight: '600',
-    fontSize: 15,
-    color: Colors.text,
-    alignSelf: 'center',
-  },
-  itemLogo: {
-    margin: 27,
-    height: 70,
     alignSelf: 'center',
   },
 });
---
+++ Screens/WorkoutTabs.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {Abs, Arm, Back, Chest, Rear, WorkoutIcons} from '../img/WorkoutIcons';
+import Colors from '../Themes/Colors';
+
+const numColumns = 2;
+
+const cardioData = [
+  {title: 'Arms', icon: <Arm />},
+  {title: 'Abs', icon: <Abs />},
+  {title: 'Rear', icon: <Rear />},
+  {title: 'Legs', icon: <WorkoutIcons />},
+  {title: 'Back', icon: <Back />},
+  {title: 'Chest', icon: <Chest />},
+];
+
+const weightsData = [
+  {title: 'Beginner'},
+  {title: 'Intermediate'},
+  {title: 'Advanced'},
+];
+
+const renderItem = ({item, index}) => {
+  return (
+    <View Key={index} style={styles.itemContainer}>
+      <View style={styles.itemLogo}>{item.icon}</View>
+      <View>
+        <Text style={styles.itemTitle}>{item.title}</Text>
+      </View>
+    </View>
+  );
+};
+
+export const Cardio = () => {
+  return (
+    <FlatList
+      key={'_'}
+      data={cardioData}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={renderItem}
+      contentContainerStyle={styles.gridView}
+      columnWrapperStyle={{justifyContent: 'space-between'}}
+      spacing={20}
+      numColumns={numColumns}
+    />
+  );
+};
+
+export const Weights = () => {
+  return (
+    <FlatList
+      key={'#'}
+      data={weightsData}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()}
+      numColumns={numColumns}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: Colors.card,
+    alignItems: 'center',
+    margin: 9,
+    width: '45%',
+    height: 160,
+  },
+  itemTitle: {
+    fontWeight: '600',
+    fontSize: 15,
+    color: Colors.text,
+    alignSelf: 'center',
+  },
+  itemLogo: {
+    margin: 27,
+    height: 70,
+    alignSelf: 'center',
+  },
+});
