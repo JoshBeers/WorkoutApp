073c1eaa5d784115566753bbdebc1770914a1fab
Anne <ajoseph13@ggc.edu>
2020-10-12 20:51:09 -0400

    cleaned up

--- Screens/ChooseWorkoutScreen.js
+++ Screens/ChooseWorkoutScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Colors from '../Themes/Colors';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 import {Cardio} from './WorkoutTabs';
