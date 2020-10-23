cf21f9e8dc1c12cc36c0e81affd3d1a411f96793
ChrisArackal <arackalc@gmail.com>
2020-10-23 18:25:24 -0400

    added a method to use sql to get routines on savedRoutineScreen

--- Screens/SavedRoutinesScreen.js
+++ Screens/SavedRoutinesScreen.js
@@ -1,8 +1,28 @@
+/* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
 import {StyleSheet, TextInput, Text, View, Button} from 'react-native';
 import {Card} from 'react-native-elements';
+import * as SQLite from "expo-sqlite";
 
 class SavedRoutinesScreen extends Component {
+
+  //method returns a list of routines
+  //has not been tested
+  getRoutines(){
+    const db = SQLite.openDatabase("workoutAppDB.db");
+
+    db.transaction(tx =>{
+      tx.executeSql("select * from routines;",[],(_,rows) =>{
+
+        console.log("sqllog_ChooseWorkoutScreen_routines", rows.rows)
+
+        return rows.rows;
+      })
+    })
+
+  } 
+
+  
     render() {
     }
 }
