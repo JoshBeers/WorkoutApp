d368306863fee3b6136b127b6f8dac08bd63984f
ChrisArackal <arackalc@gmail.com>
2020-11-04 13:56:16 -0500

    fixed index.js app registery so that icons work for expo and not react native

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import 'react-native-gesture-handler';
 
-import {AppRegistry} from 'react-native';
+//import {AppRegistry} from 'react-native';
+import { registerRootComponent } from 'expo'
 import App from './App';
 
-AppRegistry.registerComponent('main', () => App);
+registerRootComponent(App);
+//AppRegistry.registerComponent('main', () => App);
