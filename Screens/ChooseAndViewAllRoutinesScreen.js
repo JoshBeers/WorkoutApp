e42d9ea0023db85ebf20c15decb51cf650b29ef6
Josh Beers <joshbeers999@gmail.com>
2020-11-09 15:16:08 -0500

    merged;

--- Screens/ChooseAndViewAllRoutinesScreen.js
+++ Screens/ChooseAndViewAllRoutinesScreen.js
@@ -33,41 +33,6 @@ export default class ChooseAndViewAllRoutinesScreen extends React.Component {
       });
   }
 
-<<<<<<< HEAD
-  render() {
-    const savedData = [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-      {
-        id: '57694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Fourth Item',
-      },
-      {
-        id: '56694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Fifth Item',
-      },
-      {
-        id: '55694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Sixth Item',
-      },
-    ];
-
-=======
-  seeDetails(id){
-        // this.props.navigator.push("ViewDetails");
-  }
->>>>>>> 29b958a4035aab5acae21a63aa1ea51c0836ef7a
-
   render() {
       return (
         <View style={listStyle.screen}>
